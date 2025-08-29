1771fda3234fcfb959932183a676d4c05748bfcf
erika2knd <erikakondratjeva@gmail.com>
2025-08-29 14:14:52 +0300

    typing correction

--- components/NotFoundAnim.tsx
+++ components/NotFoundAnim.tsx
@@ -3,16 +3,34 @@
 import * as React from "react";
 import Lottie from "lottie-react";
 
+type LottieJSON = Record<string, unknown>;
+
 export default function NotFoundAnim() {
-  const [data, setData] = React.useState<any>(null);
+  const [data, setData] = React.useState<LottieJSON | null>(null);
 
   React.useEffect(() => {
-    fetch("/404.json")      
-      .then((r) => r.json())
-      .then(setData)
-      .catch(() => setData(null));
+    let mounted = true;
+    fetch("/404.json")
+      .then((r) => r.json() as Promise<LottieJSON>)
+      .then((json) => {
+        if (mounted) setData(json);
+      })
+      .catch(() => {
+      });
+    return () => {
+      mounted = false;
+    };
   }, []);
 
   if (!data) return null;
-  return <Lottie animationData={data} loop autoplay className="w-72 h-72 md:w-96 md:h-96" />;
+
+  return (
+    <Lottie
+      animationData={data}
+      loop
+      autoplay
+      className="w-72 h-72 md:w-96 md:h-96"
+    />
+  );
 }
+
